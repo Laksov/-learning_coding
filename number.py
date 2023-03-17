0ea2e8baf6cd355b2e882310c411a55647ff30ae
Laksov <10101928vasa@gmail.com>
2023-03-17 20:39:26 +0500

    рефакторинг

--- number.py
+++ number.py
@@ -1,11 +1,11 @@
 #https://ulearn.me/course/python/O_chisle_e81cb7d6-ba5b-4af1-aa16-eb63ec5dac7c
 n = int(input())
 count = 0
-sharing = False
+is_sharing = True
 
-while not sharing:
+while is_sharing:
     if n % 9 == 0:
-        sharing = True
+        is_sharing = False
         break
     n += 1
     count += 1
