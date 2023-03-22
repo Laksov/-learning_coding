cd2974a563524c71d92f54d43315436b52655278
Laksov <10101928vasa@gmail.com>
2023-03-22 19:02:37 +0500

    рефакторинг

--- good_string.py
+++ good_string.py
@@ -1,10 +1,15 @@
 # https://ulearn.me/course/python/Khoroshaya_stroka_0d33b814-6cb5-4bd5-bb80-9101b280a135
 
-n = list(map(int, input().split()))
+s = input()
 
-for i in range(len(n)):
-    while n[i] != 'A':
-        if n[i] != 'A':
-            n = n[1:]
+if not ('A' in s):
+    print('')
+    exit(0)
 
-print(n)
+k = 0
+
+while s[k] != 'A':
+    k += 1
+
+print(s[k:])
+
