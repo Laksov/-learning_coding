a1b8f1d90c000a3e14931b9032f4cf5f806b6187
Laksov <10101928vasa@gmail.com>
2023-03-22 19:24:08 +0500

    решил задачу 'Блэкджек'

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,17 @@
 # https://ulearn.me/course/python/Blekdzhek_a5f1a71f-68a1-4fa8-a9a0-a4355fb309c2
 
-li = list(map(int, input().split()))
 count = 0
 
-while count < 21:
-    pass
+cards = 0
+
+while count <= 21:
+    s = input()
+    if s == 'J' or s == 'Q' or s == 'K':
+        count += 10
+    elif s == 'A':
+        count += 11
+    else:
+        count += int(s)
+    cards += 1
+
+print(cards - 1)
