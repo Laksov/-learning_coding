6f7b6b82f57a9bcea3909f9274d047bc4dec3018
Laksov <10101928vasa@gmail.com>
2023-03-22 19:49:39 +0500

    решил задачу 'Диалоговая система'

--- dialogue_system.py
+++ dialogue_system.py
@@ -1,12 +1,11 @@
 # https://ulearn.me/course/python/Dialogovaya_sistema_f18cde75-bca4-4c5b-b734-46eb00b5a8de
 
 a = 0
-k = list(map(int, input().split()))
-count = 0
 
-for i in range(len(k)):
-    while a < 32 or k[i] != 0:
-        a = a + k[i]
-        count += 1
+while a <= 32:
+    n = int(input())
+    a += 1
+    if n == 0:
+        break
 
-print(count)
+print(a)
