fe86bbf04c352a1d46eee599a119491b95fdf7d7
Laksov <10101928vasa@gmail.com>
2023-04-26 19:48:18 +0500

    решил задачу на трибоначи

---
+++ tribonacci.py
@@ -0,0 +1,12 @@
+"https://ulearn.me/course/python/Tribonachchi_7d25e08b-0b2c-4822-bcc0-feedd62838ff"
+
+
+def treb(n):
+    if n <= 1:
+        return 1
+    return treb(n - 1) + treb(n - 2) * 3
+
+
+n = int(input())
+ans = treb(n)
+print(ans)
